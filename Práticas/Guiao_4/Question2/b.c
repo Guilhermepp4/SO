7f1cdcdc4f90bb06d68631950d5c2fe5a3bab8bc
Guilherme Pinho <127632355+Guilhermepp4@users.noreply.github.com>
2025-02-24 20:20:31 +0000

    Add files via upload

---
+++ Práticas/Guiao_4/Question2/b.c
@@ -0,0 +1,50 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <time.h>
+
+#define number 3
+
+int main(){
+
+    pid_t pid;
+    int fd[2];
+
+    if(pipe(fd) == -1) _exit(EXIT_FAILURE);
+
+    if((pid = fork()) == -1) _exit(EXIT_FAILURE);
+
+    if(pid == 0){
+        
+        if(close(fd[0]) == -1) _exit(EXIT_FAILURE);
+
+        srand(time(NULL));
+        int y = rand() % 100;
+        
+        for(int i = 0; i < number; i++){
+            write(fd[1], &y, sizeof(int));
+            printf("The process, %d, write the number %d, to the process %d\n\n", getpid(), y, getppid());
+        }
+        if(close(fd[1]) == -1) _exit(EXIT_FAILURE);
+        
+        _exit(0);
+    }
+    if(close(fd[1]) == -1) _exit(EXIT_FAILURE);
+    
+    int x;
+    
+    while(read(fd[0], &x, sizeof(int)) > 0){
+        printf("The process, %d, read the number %d\n\n", getpid(), x);
+    }
+    printf("EOF\n\n");
+    
+    
+    if(close(fd[0]) == -1) _exit(EXIT_FAILURE);
+    wait(NULL);
+
+    _exit(0);
+}
+
+
